af5663b1f5eb4f3eadb22cff7ce0b07033e02fe0
Mark Hunt <evilernie0@gmail.com>
2011-06-22 17:53:00 -0400

    added functions for sorting by hue

--- Processor.class.php
+++ Processor.class.php
@@ -33,8 +33,12 @@ class Processor {
     foreach ($matches as $match) {
       $c[] = $this->hex2RGB($match[0], true);
     }
-    $c = array_unique($c);
-    $d = $this->rgb2Hex($c);
+    $c = array_filter($c);
+    foreach ($c as $color) {
+      list($r, $g, $b) = explode(',', $color);
+      $hsv[] = $this->rgbtohsv($r,$g,$b);
+    }
+    $d = $this->sortColors($hsv);
     $this->colors = $d;
   }
 
@@ -78,6 +82,85 @@ class Processor {
         return false; //Invalid hex color code
     }
     return $returnAsString ? implode($seperator, $rgbArray) : $rgbArray; // returns the rgb string or the associative array
+  }
+
+  private function rgbtohsv($r,$g,$b) {
+    //Convert RGB to HSV
+    $r /= 255;
+    $g /= 255;
+    $b /= 255;
+    $min = min($r,$g,$b);
+    $max = max($r,$g,$b);
+
+    switch($max) {
+      case 0:
+          $h = $s = $v = 0;
+          break;
+      case $min:
+          $h = $s = 0;
+          $v = $max;
+          break;
+      default:
+          $delta = $max - $min;
+          if($r == $max) {
+              $h = 0 + ($g - $b) / $delta;
+          } elseif($g == $max) {
+              $h = 2 + ($b - $r) / $delta;
+          } else {
+              $h = 4 + ($r - $g) / $delta;
+          }
+          $h *= 60;
+          if($h < 0 ) $h += 360;
+          $s = $delta / $max;
+          $v = $max;
+      }
+    return array($h,$s,$v);
+  }
+
+  private function hsvtorgb($h,$s,$v) {
+    //Convert HSV to RGB
+    if($s == 0) {
+        $r = $g = $b = $v;
+    } else {
+        $h /= 60.0;
+        $s = $s;
+        $v = $v;
+
+        $hi = floor($h);
+        $f = $h - $hi;
+        $p = ($v * (1.0 - $s));
+        $q = ($v * (1.0 - ($f * $s)));
+        $t = ($v * (1.0 - ((1.0 - $f) * $s)));
+
+        switch($hi) {
+            case 0: $r = $v; $g = $t; $b = $p; break;
+            case 1: $r = $q; $g = $v; $b = $p; break;
+            case 2: $r = $p; $g = $v; $b = $t; break;
+            case 3: $r = $p; $g = $q; $b = $v; break;
+            case 4: $r = $t; $g = $p; $b = $v; break;
+            default: $r = $v; $g = $p; $b = $q; break;
+        }
+    }
+    return array(
+        (integer) ($r * 255 + 0.5),
+        (integer) ($g * 255 + 0.5),
+        (integer) ($b * 255 + 0.5),
+    );
+  }
+
+  private function sortColors($hsv) {
+    foreach($hsv as $k => $v) {
+      $hue[$k] = $v[0];
+      $sat[$k] = $v[1];
+      $val[$k] = $v[2];
+    }
+    array_multisort($hue,SORT_ASC,$sat,SORT_ASC,$val,SORT_ASC,$hsv,SORT_ASC);
+    foreach($hsv as $k => $v) {
+      list($hue,$sat,$val) = $v;
+      list($r,$g,$b) = $this->hsvtorgb($hue,$sat,$val);
+      $rgbvals[] = implode(',', array($r, $g, $b));
+    }
+    return $this->rgb2Hex($rgbvals);
   }
 }
 ?>
